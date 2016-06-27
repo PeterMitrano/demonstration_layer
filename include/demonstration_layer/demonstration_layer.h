622c49d21ed6fd36753b702c4a7b3b61810af01c
Peter Mitrano <mitranopeter@gmail.com>
2016-06-27 15:02:00 -0400

    First pass at demo layer works
     - removed other demo layer example
     - using c++11
     - subscribe to path and track which points on the map have been a part
       of demonstration at any time. unsing unorded set for this
     - plugin successfully loaded using turtlebot_move_base.launch

--- include/demonstration_layer/demonstration_layer.h
+++ include/demonstration_layer/demonstration_layer.h
@@ -1,27 +1,40 @@
 #pragma once
 
-#include <costmap_2d/GenericPluginConfig.h>
 #include <costmap_2d/layer.h>
 #include <costmap_2d/layered_costmap.h>
+#include <costmap_2d/GenericPluginConfig.h>
 #include <dynamic_reconfigure/server.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <demonstration_layer/demo_pose_stamped.h>
+#include <nav_msgs/Path.h>
 #include <ros/ros.h>
+#include <unordered_set>
 
-namespace demonstration_layer_namespace
+namespace demonstration_layer
 {
-class DemonstrationLayer : public costmap_2d::Layer
+
+class DemonstrationLayer : public costmap_2d::Layer, public costmap_2d::Costmap2D
 {
 public:
   DemonstrationLayer();
 
   virtual void onInitialize();
-  virtual void updateBounds(double robot_x, double robot_y, double robot_yaw, double* min_x, double* min_y,
-                            double* max_x, double* max_y);
+  virtual void updateBounds(double robot_x, double robot_y, double robot_yaw, double* min_x, double* min_y, double* max_x,
+                             double* max_y);
   virtual void updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i, int max_j);
+  bool isDiscretized()
+  {
+    return true;
+  }
+
+  virtual void matchSize();
 
 private:
-  void reconfigureCB(costmap_2d::GenericPluginConfig& config, uint32_t level);
+  void reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level);
+  dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig> *dsrv_;
+  ros::Subscriber demo_path_sub_;
+  std::unordered_set<DemoPoseStamped> path_set_;
 
-  double mark_x_, mark_y_;
-  dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>* dsrv_;
+  void demoPathCallback(const nav_msgs::Path& msg);
 };
 }
--- src/demonstration_layer.cpp
+++ src/demonstration_layer.cpp
@@ -1,57 +1,96 @@
 #include <demonstration_layer/demonstration_layer.h>
 #include <pluginlib/class_list_macros.h>
 
-PLUGINLIB_EXPORT_CLASS(demonstration_layer_namespace::DemonstrationLayer, costmap_2d::Layer)
+PLUGINLIB_EXPORT_CLASS(demonstration_layer::DemonstrationLayer, costmap_2d::Layer)
 
 using costmap_2d::LETHAL_OBSTACLE;
+using costmap_2d::NO_INFORMATION;
 
-namespace demonstration_layer_namespace
+namespace demonstration_layer
 {
-DemonstrationLayer::DemonstrationLayer()
-{
-}
+
+DemonstrationLayer::DemonstrationLayer() {}
 
 void DemonstrationLayer::onInitialize()
 {
-  ros::NodeHandle nh("~/" + name_);
+  ros::NodeHandle private_nh("~/" + name_);
+  ros::NodeHandle nh;
   current_ = true;
+  default_value_ = NO_INFORMATION;
+  matchSize();
 
-  dsrv_ = new dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>(nh);
-  dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>::CallbackType cb =
-      boost::bind(&DemonstrationLayer::reconfigureCB, this, _1, _2);
+  dsrv_ = new dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>(private_nh);
+  dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>::CallbackType cb = boost::bind(
+      &DemonstrationLayer::reconfigureCB, this, _1, _2);
   dsrv_->setCallback(cb);
+
+  demo_path_sub_ = nh.subscribe("demo_path", 10, &DemonstrationLayer::demoPathCallback, this);
 }
 
-void DemonstrationLayer::reconfigureCB(costmap_2d::GenericPluginConfig& config, uint32_t level)
+
+void DemonstrationLayer::matchSize()
+{
+  Costmap2D* master = layered_costmap_->getCostmap();
+  resizeMap(master->getSizeInCellsX(), master->getSizeInCellsY(), master->getResolution(),
+            master->getOriginX(), master->getOriginY());
+}
+
+
+void DemonstrationLayer::reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level)
 {
   enabled_ = config.enabled;
 }
 
-void DemonstrationLayer::updateBounds(double robot_x, double robot_y, double robot_yaw, double* min_x, double* min_y,
-                                      double* max_x, double* max_y)
+void DemonstrationLayer::demoPathCallback(const nav_msgs::Path& msg)
 {
-  if (!enabled_)
-    return;
+  ROS_INFO_ONCE("demo path recieved!");
+  for (auto pose_stamped : msg.poses)
+  {
+    DemoPoseStamped demo_pose(layered_costmap_->getCostmap(), pose_stamped);
+    path_set_.insert(demo_pose);
+  }
 
-  mark_x_ = robot_x + cos(robot_yaw);
-  mark_y_ = robot_y + sin(robot_yaw);
-
-  *min_x = std::min(*min_x, mark_x_);
-  *min_y = std::min(*min_y, mark_y_);
-  *max_x = std::max(*max_x, mark_x_);
-  *max_y = std::max(*max_y, mark_y_);
+  ROS_INFO("demo point set size: %lu", path_set_.size());
 }
 
-void DemonstrationLayer::updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i, int max_j)
+void DemonstrationLayer::updateBounds(double robot_x, double robot_y, double robot_yaw, double* min_x,
+                                           double* min_y, double* max_x, double* max_y)
 {
   if (!enabled_)
     return;
-  unsigned int mx;
-  unsigned int my;
-  if (master_grid.worldToMap(mark_x_, mark_y_, mx, my))
+
+  //double mark_x = robot_x + cos(robot_yaw), mark_y = robot_y + sin(robot_yaw);
+  //unsigned int mx;
+  //unsigned int my;
+  //if(worldToMap(mark_x, mark_y, mx, my)){
+    //setCost(mx, my, LETHAL_OBSTACLE);
+  //}
+
+  //*min_x = std::min(*min_x, mark_x);
+  //*min_y = std::min(*min_y, mark_y);
+  //*max_x = std::max(*max_x, mark_x);
+  //*max_y = std::max(*max_y, mark_y);
+}
+
+void DemonstrationLayer::updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i,
+                                          int max_j)
+{
+  if (!enabled_)
+    return;
+
+  // go through the entire map and increase cost to non-zero value (such as 1)
+  // go through all the points in all the paths and set their cost to 0.
+
+  for (int j = min_j; j < max_j; j++)
   {
-    master_grid.setCost(mx, my, LETHAL_OBSTACLE);
+    for (int i = min_i; i < max_i; i++)
+    {
+      //int index = getIndex(i, j);
+      //if (costmap_[index] == NO_INFORMATION)
+        //continue;
+      //master_grid.setCost(i, j, costmap_[index]);
+    }
   }
 }
 
-}  // end namespace
+} // end namespace
